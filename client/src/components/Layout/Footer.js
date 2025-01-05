8f0fa6149d363221f5583fa5067e49663a45e42e
ajeet gupta <140450935+AJKakarot@users.noreply.github.com>
2025-01-05 23:44:56 +0530

    ready

---
+++ client/src/components/Layout/Footer.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div
+      className="bg-dark text-light p-4"
+      style={{
+        position: "fixed",
+        bottom: "0",
+        width: "100%",
+        marginBottom: "0",
+      }}
+    >
+      <h6 className="text-center text-white">
+        All rights reserved &copy; $AJ
+      </h6>
+    </div>
+  );
+};
+
+export default Footer;
