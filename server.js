2c8143338d6ec5c90e1e09c3859e265ef3928b25
ajeet gupta <140450935+AJKakarot@users.noreply.github.com>
2025-01-13 18:22:41 +0530

    Fixed some issue

--- server.js
+++ server.js
@@ -3,17 +3,17 @@ const cors = require("cors");
 const morgan = require("morgan");
 const dotenv = require("dotenv");
 const colors = require("colors");
+const path = require("path"); // Import path module
 const connectDb = require("./config/connectDb");
-import path from "path";
+
 // config dot env file
 dotenv.config();
 
 //database call
 connectDb();
+
 //rest object
 const app = express();
-
-const _dirname = path.resolve();
 
 //middlewares
 app.use(morgan("dev"));
@@ -21,22 +21,20 @@ app.use(express.json());
 app.use(cors());
 
 //routes
-//user routes
 app.use("/api/v1/users", require("./routes/userRoute"));
-//transections routes
 app.use("/api/v1/transections", require("./routes/transectionRoutes"));
 
-app.use(express.static(path.join(_dirname,"/client/dist")))
-app.get('*',(_,res) => {
-  res.sendFile(path.resolve(_dirname,"client" , "dist", "index.html"))
-})
+// Serve static files (if using React client)
+app.use(express.static(path.join(__dirname, "./client/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build", "index.html"));
+});
 
 //port
-const PORT = 8080|| process.env.PORT;
-
+const PORT = process.env.PORT || 8080;
 
 //listen server
 app.listen(PORT, () => {
-
   console.log(`Server running on port ${PORT}`);
 });
